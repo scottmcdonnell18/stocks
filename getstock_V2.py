f4003587a07c9d985e1da4955d9fc8e228da7333
scottmcdonnell18 <45824455+scottmcdonnell18@users.noreply.github.com>
2020-06-14 13:42:35 +0100

    Update getstock_V2.py

--- getstock_V2.py
+++ getstock_V2.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pandas_datareader.data as web
 
 #this reads in a stock ticker from command line and checks today's stock price.
-#if there is no data available today, it checks last night's closing price.
+#if  market is currently closed, it checks last night's closing price.
 
 try:
     stock = sys.argv[1]
